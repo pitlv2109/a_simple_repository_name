75d6276859d4840f17948ecdff973cbb02546a28
Phillip Quy Le <pitlv2109@gmail.com>
2017-03-16 20:22:37 -0500

    yo world

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express')
+
+var app = express()
+
+app.use(express.static('./app/public'))
+
+app.get('/', function(req, res) {
+    res.send('Yo World')
+})
+
+app.get('/:school', function(req, res) {
+    res.send('You requested school ' + req.params.school)
+})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!')
+})
