4e86a826326368967cc654d31372f3f76b87bd11
Phillip Quy Le <pitlv2109@gmail.com>
2017-03-24 01:14:56 -0500

    Reformatted signin and signup

---
+++ public/js/signup.js
@@ -0,0 +1,16 @@
+function match(input) {
+    if (input.value != document.getElementById('password').value) {
+        input.setCustomValidity('Password Must be Matching.');
+    } else {
+        input.setCustomValidity('');
+    }
+}
+function pwd(input) {
+    if (input.value.includes(" ")) {
+        input.setCustomValidity('Must not contain spaces.');
+    } else if (input.value.length < 6) {
+        input.setCustomValidity('Minimum length is 6 characters.');
+    } else {
+        input.setCustomValidity('');
+    }
+}
